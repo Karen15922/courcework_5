41eae5bb382d849c19164a1b8cb10aa44665ebd6
Карен <karen.karakeyan@yandex.ru>
2024-06-12 19:11:10 +0300

    add commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from db_manager import create_database
+dbname='postgres'
+user='postgres'
+password='6276703zx'
+host='localhost'
+port='5432'
+
+create_database(dbname, user, password, host, port)
