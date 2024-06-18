d408c45fcf180c283a35bbdbe1efa79e213a0fe0
Карен <karen.karakeyan@yandex.ru>
2024-06-19 02:28:32 +0300

    внесены коректировки в класс db_manager для корректной функциональности

--- src/db_manager.py
+++ src/db_manager.py
@@ -15,21 +15,21 @@ class DBManager:
         self.cur = self.conn.cursor()
 
     def get_companies_and_vacancies_count(self):
-        self.cur.execute("SELECT name, COUNT(id) FROM employers  JOIN vacancies  ON id = employer_id GROUP BY name")
+        self.cur.execute("SELECT name, COUNT(vacancies.id) FROM employers  JOIN vacancies  ON employers.id = vacancies.employer_id GROUP BY name")
         table = PrettyTable()
         table.field_names = ['companies', 'vacancy_quantity']
         table.add_rows(self.cur.fetchall())
         print(table)
 
     def get_all_vacancies(self):
-        self.cur.execute("SELECT name, title, salary, url FROM employers  JOIN vacancies  ON id = employer_id")
+        self.cur.execute("SELECT name, title, salary, url FROM employers  JOIN vacancies  ON employers.id = vacancies.employer_id")
         table = PrettyTable()
         table.field_names = ['name', 'title', 'salary', 'url']
         table.add_rows(self.cur.fetchall())
         print(table)
 
     def get_avg_salary(self):
-        self.cur.execute("SELECT AVG(salary::integer) FROM employers  JOIN vacancies  ON id = employer_id")
+        self.cur.execute("SELECT AVG(salary::integer) FROM employers  JOIN vacancies  ON employers.id = vacancies.employer_id")
         table = PrettyTable()
         table.field_names = ['avg_salary']
         table.add_rows(self.cur.fetchall())
@@ -37,7 +37,7 @@ class DBManager:
 
     def get_vacancies_with_higher_salary(self):
         avg_salary = self.get_avg_salary()
-        self.cur.execute("SELECT name, title, salary, url FROM employers  JOIN vacancies  ON employers.id = vacancies.employer_id WHERE salary > (SELECT AVG(salary) FROM vacancies);")
+        self.cur.execute("SELECT name, title, salary, url FROM employers  JOIN vacancies  ON employers.id = vacancies.employer_id WHERE CAST(salary AS INTEGER) > (SELECT AVG(CAST(salary AS INTEGER)) FROM vacancies);")
         table = PrettyTable()
         table.field_names = ['name', 'title', 'salary', 'url']
         table.add_rows(self.cur.fetchall())
